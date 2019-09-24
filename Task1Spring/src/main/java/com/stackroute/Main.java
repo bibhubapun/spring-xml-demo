c676ce3a0acd848fd920a7f8c50f66b09d6e7140
bibhu.panda <bibhu.panda@cgi.com>
2019-09-24 14:47:44 +0530

    autowire

--- Task1Spring/src/main/java/com/stackroute/Main.java
+++ Task1Spring/src/main/java/com/stackroute/Main.java
@@ -6,10 +6,8 @@ public class Main
     public static void main( String[] args )
     {
         ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
-        Movie beanA=context.getBean("movieA",Movie.class);
-        Movie beanB=context.getBean("movieB",Movie.class);
-        System.out.println(beanA==beanB);
-
+        Movie movie=context.getBean("movie",Movie.class);
+        movie.display();
        /* BeanDefinitionReader reader=new XmlBeanDefinitionReader();
        reader.getResourceLoader();*/
 
--- Task1Spring/src/main/java/com/stackroute/Movie.java
+++ Task1Spring/src/main/java/com/stackroute/Movie.java
@@ -3,7 +3,11 @@ package com.stackroute;
 public class Movie {
     private Actor actor;
 
-    public Movie(Actor actor) {
+   /* public Movie(Actor actor) {
+        this.actor = actor;
+    }*/
+
+    public void setActor(Actor actor) {
         this.actor = actor;
     }
 
