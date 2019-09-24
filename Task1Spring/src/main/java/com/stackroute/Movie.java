17379b65ff6fbae4b580974b1307a44b634a93a1
bibhu.panda <bibhu.panda@cgi.com>
2019-09-24 15:35:52 +0530

    aware-interface

--- Task1Spring/src/main/java/com/stackroute/Movie.java
+++ Task1Spring/src/main/java/com/stackroute/Movie.java
@@ -1,6 +1,13 @@
 package com.stackroute;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements BeanNameAware, BeanFactoryAware, ApplicationContextAware {
     private Actor actor;
 
    /* public Movie(Actor actor) {
@@ -13,5 +20,20 @@ public class Movie {
 
     public void display(){
         System.out.println("Name: "+actor.getName()+" Gender:"+actor.getGender()+" age:"+actor.getAge());
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println(s);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
     }
 }
